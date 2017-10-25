89a8c59db3b53b282b8d79ddd08894b85fc8eddc
Zhanwen xu <leon.xu@kii.com>
2017-10-25 11:15:10 +0800

    modified test

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
 import DexOperator
 
-masterReader = DexOperator.MasterReader("/dev/ttyUSB0", "NEC1234567RR01L01")
-data = masterReader.read()
-print data
+def readData():
+    masterReader = DexOperator.MasterReader("/dev/ttyUSB0", "NEC1234567RR01L01")
+    data = masterReader.read()
+    return data
+
+def main():
+    gotData = False
+    while not gotData:
+        result = readData()
+        if result !== False:
+            gotData = True
+            print result
+
+if __name__ == '__main__':
+    main()
 
 # slaveReader = DexOperator.SlaveReader("/dev/tty.usbserial")
 # data = slaveReader.read()
