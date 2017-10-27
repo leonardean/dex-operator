68a2cd1086f7d2218e501a34f373b2569f77549b
Zhanwen xu <leon.xu@kii.com>
2017-10-27 16:05:59 +0800

    get write result

--- write.py
+++ write.py
@@ -3,5 +3,5 @@ import DexOperator
 dumdata = "DXS*NEC0000000*VA*V0/6*1\nST*001*0001\nPC1*2*5\nPC1*3*10\nG85*12234\nSE*1*0001\nDXE*1*1\n"
 
 masterWriter = DexOperator.MasterWriter("/dev/ttyUSB0", "NEC1234567SR01L01")
-result masterWriter.write(dumdata)
+result = masterWriter.write(dumdata)
 print result
