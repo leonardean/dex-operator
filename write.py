7951fac902638d3d863c18945d76624606ac58d9
Zhanwen xu <leon.xu@kii.com>
2017-10-27 16:20:40 +0800

    id change

--- write.py
+++ write.py
@@ -2,6 +2,6 @@ import DexOperator
 
 dumdata = "DXS*NEC0000000*VA*V0/6*1\nST*001*0001\nPC1*2*5\nPC1*3*10\nG85*12234\nSE*1*0001\nDXE*1*1\n"
 
-masterWriter = DexOperator.MasterWriter("/dev/ttyUSB0", "NEC1234567SR01L01")
+masterWriter = DexOperator.MasterWriter("/dev/ttyUSB0", "NEC1234567SR00L06")
 result = masterWriter.write(dumdata)
 print result
